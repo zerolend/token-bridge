2f95995ea3caa4010b6d8800c38da10a1e942c88
Deadshot Ryker <ryker@zerolend.xyz>
2024-05-22 17:52:32 +0400

    Added pause fn

--- contracts/ZeroOFT.sol
+++ contracts/ZeroOFT.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
-import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract ZeroOFT is OFT, ERC20Permit {
     mapping(address => bool) public blacklist;
     mapping(address => bool) public permitWhitelist;
+    bool public paused;
     bool public enablePermitWhitelist;
 
     constructor(
         address _lzEndpoint
-    ) OFT("ZeroLend", "ZERO", _lzEndpoint, msg.sender) Ownable(msg.sender) ERC20Permit("ZeroLend") {
+    )
+        OFT("ZeroLend", "ZERO", _lzEndpoint, msg.sender)
+        Ownable(msg.sender)
+        ERC20Permit("ZeroLend")
+    {
         // nothing
     }
 
@@ -25,7 +30,8 @@ contract ZeroOFT is OFT, ERC20Permit {
         bytes32 r,
         bytes32 s
     ) public virtual override {
-        if (enablePermitWhitelist) require(permitWhitelist[spender], "!whitelist permit");
+        if (enablePermitWhitelist)
+            require(permitWhitelist[spender], "!whitelist permit");
         super.permit(owner, spender, value, deadline, v, r, s);
     }
 
@@ -37,13 +43,22 @@ contract ZeroOFT is OFT, ERC20Permit {
         blacklist[who] = what;
     }
 
+    function togglePause(bool what) external onlyOwner {
+        paused = what;
+    }
+
     function toggleWhitelistPermit(bool what) external onlyOwner {
         enablePermitWhitelist = what;
     }
 
-    function _update(address from, address to, uint256 value) internal virtual override {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
         require(!blacklist[from], "blacklisted");
         require(!blacklist[to], "blacklisted");
+        require(!paused, "paused");
         super._update(from, to, value);
     }
 }
